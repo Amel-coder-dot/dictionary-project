5506088f68b42d05a09ed26e173e66d42d6bf797
Amel-coder-dot <collie.freya@gmail.com>
2026-02-20 07:53:00 +0000

    Fixed an Issue

--- src/Photos.js
+++ src/Photos.js
@@ -10,8 +10,12 @@ export default function Photos(props) {
             console.log(props.photos);
             return (
               <div className="col-4" key={index}>
-                <a href={photo.src.orginal} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} className="img-fluid" alt="" />
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.alt}
+                  />
                 </a>
               </div>
             );
